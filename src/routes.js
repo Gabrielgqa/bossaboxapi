c7d67d71452597e240adb681ee5070ecbdfabee3
GabrielGQA <gabrielgqa@gmail.com>
2019-12-24 09:53:48 -0300

    configura eslint, prettier e editorConfig

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hi Gabe!!' });
-})
+routes.get('/', (req, res) => res.json({ message: 'Hi Gabe!!' }));
 
 export default routes;
